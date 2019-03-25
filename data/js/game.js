216a7f51454b3c8323b7e0e9f0737d5698659586
alurosu <alurosu@gmail.com>
2019-03-25 14:47:09 +0200

    initial

---
+++ data/js/game.js
@@ -0,0 +1,384 @@
+var appPaused = true;
+
+var cWidth;	  //canvas width
+var cHeight;  //canvas height
+var ctx; 		  //canvas context
+var canvas;
+var then;
+var countToAds = 0;
+var player;
+var enemies = new Array();
+var t;
+var c;
+var score = 0;
+var notification;
+var globalR;
+var snd = null;
+var sndScore = null;
+var sndCoin = null;
+var canBuy = false;
+var notUsedInv = true;
+var noMargin = true;
+var coinsThisGame = 0;
+var enemyKills = 0;
+
+if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry)/)) {
+	document.addEventListener("deviceready", onDeviceReady, false);
+} else {
+	$(document).ready(function(){
+		onDeviceReady();
+	});
+}
+
+function onDeviceReady(){
+	gpurl = getPhoneGapPath();
+	if (typeof Media != 'undefined') {
+		snd = new Media(gpurl + "data/audio/background.mp3", onSuccess, onError, onStatus);
+		
+		sndScore = new Media(gpurl + "data/audio/hit.mp3");
+		sndCoin = new Media(gpurl + "data/audio/pu1.mp3");
+		
+		if (localStorage.sound == 'off') {
+			//sound is off
+			$('#volume .fa').removeClass('fa-volume-up').addClass('fa-volume-off');
+			snd.stop();
+		} else {
+			//sound is on or default
+			localStorage.sound == 'on';
+			$('#volume .fa').removeClass('fa-volume-off').addClass('fa-volume-up');
+			snd.play();
+		}
+	}
+	
+	if (typeof inappbilling != 'undefined') {
+		inappbilling.init(function(){
+			canBuy = true;
+			
+			inappbilling.getPurchases(function(result){
+				if (result.length > 0){
+				}					
+			}, function(error){console.log("ERROR: \r\n" + error);});
+		}, function(error){
+			console.log("ERROR: \r\n" + error);
+			$("#buyCoins").fadeOut(0);
+		});
+	}
+	
+	if (!localStorage.coins)
+		localStorage.coins = 0;
+	
+	if (!localStorage.isLogin)
+		localStorage.isLogin = 'true';
+	
+	if (!localStorage.highscore)
+		localStorage.highscore = 0;
+	$('#highscore').html('Best<span>:</span>'+localStorage.highscore);
+	
+	if (localStorage.isLogin == 'true') {
+		$('#autoLogin .fa').removeClass('fa-user-times').addClass('fa-user');
+		$('#autoLogin span').html('on');
+		doLogin();
+	} else {
+		$('#autoLogin .fa').removeClass('fa-user').addClass('fa-user-times');
+		$('#autoLogin span').html('off');
+	}
+	
+	init(); 
+	var clickHandler = ('ontouchstart' in document.documentElement ? "touchstart" : "mousedown");
+	$('canvas').on(clickHandler, function(e) {
+		var x = e.originalEvent.touches?e.originalEvent.touches[0].pageX:e.clientX;
+		var y = e.originalEvent.touches?e.originalEvent.touches[0].pageY:e.clientY;
+		player.setTarget(x,y);		
+		
+		if (!t) {
+			t = new target();
+			t.spawn();
+			notification = 'catch the blue dot';
+		}
+	});
+	
+	$('#rate').on(clickHandler, function(e) {
+		openBrowser("market://details?id=alurosu.games.catchthedot");
+	});
+	
+	$('#about').on(clickHandler, function(e) {
+		openBrowser("http://bit.ly/SpringForce");
+	});
+	
+	$('#bomb').on(clickHandler, function(e) {
+		if (localStorage.coins<5)
+			notification = 'you need 5 coins';
+		else if (enemies.length<5)
+			notification = 'you need 5 enemies';
+		else if (!player.invulnerability) {
+			localStorage.coins -= 5;
+			$('#bomb').addClass('disabled');
+			notification = '     ';
+			player.setInvulnerability();
+			notUsedInv = false;
+		}
+	});
+	
+	$('#share').on(clickHandler, function(e) {
+		navigator.share("My best score in Catch the Dot is " + localStorage.highscore + "! Can you beat me? :D http://bit.ly/CatchTheDot","Catch the Dot");
+	});
+	
+	$('#buyCoins').on(clickHandler, function(e) {
+		if (canBuy) {
+			inappbilling.buy(function(){
+				inappbilling.consumePurchase(function(){
+					localStorage.coins = parseInt(localStorage.coins) + 50;
+					alert("You now own " + localStorage.coins + " coins.");
+					// add helper achievement
+					doAchievement("CgkI_7ufk-EKEAIQCg");
+					
+					if (localStorage.coins>=50) doAchievement("CgkI_7ufk-EKEAIQDA");
+					if (localStorage.coins>=200) doAchievement("CgkI_7ufk-EKEAIQDg");
+					if (localStorage.coins>=500) doAchievement("CgkI_7ufk-EKEAIQDQ");
+				
+				}, function(error){console.log("ERROR: \r\n" + error);}, "50coins")
+			}, function(error){console.log("ERROR: \r\n" + error);}, "50coins");
+		}
+	});
+	
+	$('#settings').on(clickHandler, function(e) {
+		$('#menu ul').fadeToggle();
+		e.stopPropagation();
+	});
+	
+	$('#menu').on(clickHandler, function (e){
+		//close settings
+		adsClicked = false;
+		var container = $("#menu ul");
+		if (!container.is(e.target) && container.has(e.target).length === 0) {
+			container.fadeOut();
+		}
+	});
+	
+	$('#volume').on(clickHandler, function(e) {
+		if (typeof Media != 'undefined') {
+			if (localStorage.sound == 'on') {
+				//sound goes off
+				localStorage.sound = 'off';
+				$('#volume .fa').removeClass('fa-volume-up').addClass('fa-volume-off');
+				snd.pause();
+			} else {
+				//sound goes on
+				localStorage.sound = 'on';
+				$('#volume .fa').removeClass('fa-volume-off').addClass('fa-volume-up');
+				snd.play();
+			}
+		}
+	});
+	
+	$("#leaderboard").on(clickHandler, function(e) {
+		if (localStorage.isLogin == 'true') {
+			var dt = {
+				leaderboardId: "CgkI_7ufk-EKEAIQAQ"
+			};
+			googleplaygame.showLeaderboard(dt);
+		}
+	});
+	
+	$("#achievements").on(clickHandler, function(e) {
+		if (localStorage.isLogin == 'true') {
+			googleplaygame.showAchievements();
+		}
+	});
+	
+	$('#play').on(clickHandler, function(e) {
+		$('#menu').fadeOut();
+		startGame();
+	});
+	
+	$('#fullAds .close div').on(clickHandler, function(e) {
+		$('#fullAds').fadeOut(0);
+	});
+	
+	$('#autoLogin').on(clickHandler, function(e) {
+		if (localStorage.isLogin == 'false') {
+			$('#autoLogin .fa').removeClass('fa-user-times').addClass('fa-user');
+			$('#autoLogin span').html('on');
+			localStorage.isLogin = 'true';
+		}	else {
+			$('#autoLogin .fa').removeClass('fa-user').addClass('fa-user-times');
+			$('#autoLogin span').html('off');
+			$('.hidden').fadeOut(0);
+			localStorage.isLogin = 'false';
+		}
+	});
+	
+}
+
+function init() {
+	canvas = $("#canvas").get(0);
+	canvas.width = cWidth = $("body").width();
+	canvas.height = cHeight = $("body").height();
+	$('#canvas').height(cHeight);
+	fontFix();
+	
+	ctx = canvas.getContext("2d");
+	var w = window;
+	requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame || function(f){window.setTimeout(f,1000/60)};
+}
+
+function loop() {
+	ctx.beginPath();
+	ctx.fillStyle = "#112435";
+	ctx.fillRect(0, 0, cWidth, cHeight);
+	
+	var now = Date.now();
+	var delta = now - then;
+	then = now;
+	update(delta / 1000);
+
+	if(appPaused == false)
+		requestAnimationFrame(loop);
+};
+
+function update(modifier) {
+	drawCoins();
+	drawScore();
+	drawNotification(notification);
+	player.draw(modifier);
+	
+	if (t) {
+		if (Math.sqrt((t.x-player.x)*(t.x-player.x) + (t.y-player.y)*(t.y-player.y)) < t.r+player.r) {
+			enemies.push(new enemy());
+			if (localStorage.coins>=5 && enemies.length>=5 && !player.invulnerability)
+				$('#bomb').removeClass('disabled');
+			if (enemies.length<2)
+				notification = 'avoid the red dots';
+			else if (!player.invulnerability) notification = '';
+			if (!c && enemies.length%3==2) {
+				c = new coin();
+				c.spawn();
+				setTimeout(function(){ c=null; }, 3000);
+			}
+			score++;
+			if (localStorage.sound == "on" && typeof Media != 'undefined')
+				sndScore.play();
+			if (score == 10) doAchievement("CgkI_7ufk-EKEAIQAw");
+			else if (score == 15) {
+				doAchievement("CgkI_7ufk-EKEAIQBA");
+				if (notUsedInv)
+					doAchievement("CgkI_7ufk-EKEAIQDw");
+				if (noMargin)
+					doAchievement("CgkI_7ufk-EKEAIQBw");
+			}
+			else if (score == 20) doAchievement("CgkI_7ufk-EKEAIQBQ");
+			else if (score >= 25) doAchievement("CgkI_7ufk-EKEAIQBg");
+			
+			t.spawn();
+		}
+		t.draw();
+	}
+	if (c) {
+		if (Math.sqrt((c.x-player.x)*(c.x-player.x) + (c.y-player.y)*(c.y-player.y)) < c.r+player.r) {
+			c.x=c.y=-2*player.r;
+			localStorage.coins++;
+			coinsThisGame++;
+			if (localStorage.sound == "on" && typeof Media != 'undefined')
+				sndCoin.play();
+			if (coinsThisGame>=5)	doAchievement("CgkI_7ufk-EKEAIQCA");
+			if (localStorage.coins>=50) doAchievement("CgkI_7ufk-EKEAIQDA");
+			else if (localStorage.coins>=200) doAchievement("CgkI_7ufk-EKEAIQDg");
+			else if (localStorage.coins>=500) doAchievement("CgkI_7ufk-EKEAIQDQ");
+			
+			if (localStorage.coins>=5 && enemies.length>=5 && !player.invulnerability)
+				$('#bomb').removeClass('disabled');
+		} else c.draw(modifier);
+	}
+	
+	for (var i=0; i<enemies.length; i++) {
+		enemies[i].draw(modifier);
+		if (Math.sqrt((enemies[i].x-player.x)*(enemies[i].x-player.x) + (enemies[i].y-player.y)*(enemies[i].y-player.y)) < enemies[i].r+player.r) {
+			if (player.invulnerability == true) {
+				//destroy enemy
+				if (localStorage.sound == "on" && typeof Media != 'undefined') {
+					var sndBum = new Media(gpurl + "data/audio/bum.mp3");
+					sndBum.play();
+				}
+				enemies.splice(i, 1);
+				if (enemies.length<5) {
+					$('#bomb').addClass('disabled');
+				}
+				enemyKills++;
+				if(enemyKills >= 5)
+					doAchievement("CgkI_7ufk-EKEAIQCQ");
+			} else {
+				//game over
+				appPaused = true;
+				notUsedInv = true;
+				noMargin = true;
+				coinsThisGame = 0;
+				$('#bomb').addClass('disabled');
+				$("#menuTitle").html("Game Over");
+				$('#endScore').html('Score<span>:</span>'+score).fadeIn(0);
+				
+				// ads
+				countToAds++;
+				if (countToAds%6==0 && !localStorage.neverRate) {
+					// rate app alert
+					navigator.notification.confirm(
+					'If you enjoy Catch the Dot, whould you mind taking a moment to rate it? It won\'t take more than a minute. Thanks for your support!',
+					function(button) {
+						if (button == '1') {    // Rate Now
+							console.log("Rate Now");
+							localStorage.neverRate = true;
+							openBrowser("market://details?id=alurosu.games.catchthedot");
+						} else if (button == '2') { // Later
+							console.log("Remind later");
+						}
+					}, 'Rate Catch the Dot', ['Rate it', 'Remind me later']);
+				} else if (countToAds%3==0) {
+					// fill ads
+					loadAds();
+					$('#fullAds').fadeIn(0);
+				}
+				
+				$('#menu').fadeIn();
+				$('#centerMenu').css({'margin-top' : cHeight*0.5-$('#centerMenu').height()*0.5});
+				if (score>localStorage.highscore) {
+					localStorage.highscore = score;
+					$('#highscore').html('Best<span>:</span>'+score);
+					if (localStorage.isLogin == 'true') {
+						//save score to google
+						submitHighscore(localStorage.highscore);
+					}
+				}
+			}
+		}
+	}
+}
+
+function openBrowser(url){
+	navigator.app.loadUrl(url, {openExternal : true});
+	return false;
+}
+document.addEventListener("pause", onPause, false);
+function onPause() {
+	snd.pause();
+	appPaused = true;
+}
+
+document.addEventListener("resume", onResume, false);
+function onResume() {
+	if (!localStorage.sound)
+		localStorage.sound = "on";
+	if (localStorage.sound == "on")
+		snd.play();
+	appPaused = false;
+	
+	then = Date.now();
+	loop();
+}
+function onStatus(status) {
+	if( status==Media.MEDIA_STOPPED ) {
+			snd.play();
+	}
+}
+
+function loadAds() {
+	$("#fillAds").attr("src","data/ads.html");
+}
---
+++ data/js/objects.js
@@ -0,0 +1,347 @@
+var ball = function (x, y){
+	this.x = x;
+	this.y = y;
+	this.r = cHeight*0.04;
+	this.speed = cHeight*0.7;
+	this.xDirection = 0;
+	this.yDirection = 0;
+	this.xTarget = x;
+	this.yTarget = y;
+	this.distance = 0;
+	this.invulnerability = false;
+	this.invulnerabilityTimer = 0;
+	this.setInvulnerability = function(){
+		this.invulnerability = true;
+		this.invulnerabilityTimer = 4;
+	}
+	this.setTarget = function(x, y){
+		this.xTarget = x;
+		this.yTarget = y;
+		this.distance = Math.sqrt((this.yTarget-this.y)*(this.yTarget-this.y) + (this.xTarget-this.x)*(this.xTarget-this.x));
+		this.xDirection = (this.xTarget-this.x)/this.distance;
+		this.yDirection = (this.yTarget-this.y)/this.distance;
+	}
+	this.drawInvulnerabilityNotification = function(){
+		var r = cHeight*0.025;
+		ctx.textBaseline="middle";
+		ctx.textAlign="center";
+		
+		ctx.beginPath();
+		ctx.font = 4*r + "px Monospace";
+		ctx.fillStyle = "#f08047";
+		ctx.fillText(this.invulnerabilityTimer, cWidth*0.5, cHeight*0.5 - r*3);
+		
+		ctx.beginPath();
+		ctx.font = 1.5*r + "px Monospace";
+		ctx.fillStyle = "#f08047";
+		ctx.fillText("Invulnerability", cWidth*0.5, cHeight*0.5 + r);
+	}
+	this.draw = function(modifier){
+		this.x += this.xDirection*this.speed*modifier;
+		if (this.x<0) {
+			this.x = 0;
+			this.xDirection *= -1;
+			noMargin = false;
+		} else if (this.x>cWidth) {
+			this.x = cWidth;
+			this.xDirection *= -1;
+			noMargin = false;
+		}
+		this.y += this.yDirection*this.speed*modifier;
+		if (this.y<0) {
+			this.y = 0;
+			this.yDirection *= -1;
+			noMargin = false;
+		} else if (this.y>cHeight) {
+			this.y = cHeight;
+			this.yDirection *= -1;
+			noMargin = false;
+		}
+		
+		ctx.beginPath();
+		if (this.invulnerability) {
+			this.invulnerabilityTimer = (this.invulnerabilityTimer-modifier).toFixed(2);
+			if (this.invulnerabilityTimer > 0) {
+				this.drawInvulnerabilityNotification();
+			} else {
+				if (localStorage.coins >= 5)
+					$('#bomb').removeClass('disabled');
+				this.invulnerabilityTimer = 0;
+				this.invulnerability = false;
+				enemyKills = 0;
+				notification = '';
+			}
+			
+			ctx.shadowBlur=50;
+			ctx.shadowColor="#f08047";
+			ctx.strokeStyle="#f08047";
+			ctx.fillStyle="#ffff80";
+			ctx.lineWidth = 2;
+		} else {
+			ctx.fillStyle="#7bd827";
+			ctx.strokeStyle="#000";
+			ctx.lineWidth = 1;
+		}
+		
+		ctx.arc(this.x, this.y,this.r,0,2*Math.PI);
+		ctx.fill();
+		ctx.stroke();
+		ctx.shadowBlur=0;
+	}
+}
+
+var target = function(){
+	this.x = 0;
+	this.y = 0;
+	this.r = cHeight*0.035;
+	this.spawn = function(){
+		this.x = Math.floor(Math.random() * cWidth) + this.r;
+		if (this.x>cWidth-this.r)
+			this.x=cWidth-this.r;
+		this.y = Math.floor(Math.random() * cHeight) + this.r;
+		if (this.y>cHeight-4*this.r)
+			this.y=cHeight-4*this.r;
+		
+		if (this.x>cWidth*0.5-2*this.r && 
+			this.x<cWidth*0.5+2*this.r && 
+			this.y>cHeight*0.5-2*this.r && 
+			this.y<cHeight*0.5+2*this.r) 
+			this.spawn(); //targets should not spawn in center
+	}
+	this.draw = function() {
+		ctx.beginPath();
+		ctx.fillStyle="#3a8bda";
+		ctx.strokeStyle="#000";
+		ctx.lineWidth = 1;
+		ctx.arc(this.x, this.y,this.r,0,2*Math.PI);
+		ctx.fill();
+		ctx.stroke();
+	}
+}
+
+var coin = function(){
+	this.x = 0;
+	this.y = 0;
+	this.r = cHeight*0.035;
+	this.glow = this.r;
+	this.glowSpeed = this.r;
+	this.spawn = function(){
+		this.x = Math.floor(Math.random() * cWidth) + this.r;
+		if (this.x>cWidth-this.r)
+			this.x=cWidth-this.r;
+		this.y = Math.floor(Math.random() * cHeight) + this.r;
+		if (this.y>cHeight-4*this.r)
+			this.y=cHeight-4*this.r;
+	}
+	this.draw = function(modifier) {
+		this.glow += modifier*this.glowSpeed;
+		if (this.glow > this.r){
+			this.glow = this.r;
+			this.glowSpeed *=-1;
+		} else if (this.glow < this.r*0.5) {
+			this.glow = this.r*0.5;
+			this.glowSpeed *=-1;
+		}
+		
+		ctx.beginPath();
+		ctx.shadowBlur=this.glow;
+		ctx.shadowColor="#ffff80";
+		ctx.fillStyle="#ffff80";
+		ctx.strokeStyle="#f08047";
+		ctx.lineWidth = 1;
+		ctx.arc(this.x, this.y,this.r,0,2*Math.PI);
+		ctx.fill();
+		ctx.stroke();
+		ctx.shadowBlur=0;
+		
+		ctx.textBaseline="middle";
+		ctx.textAlign="center";
+		ctx.fillStyle = "#f08047";
+		ctx.font = 1.2*this.r + "px Monospace";
+		ctx.fillText('$', this.x, this.y);
+	}
+}
+
+var enemy = function(){
+	this.x = cWidth - player.x;
+	this.y = cHeight - player.y;
+	this.r = cHeight*0.02;
+	this.speed = cHeight*((Math.random() * (0.7 - 0.1) + 0.1).toFixed(4));
+	this.xDirection = -player.xDirection;
+	this.yDirection = -player.yDirection;
+	
+	this.draw = function(modifier){
+		this.x += this.xDirection*this.speed*modifier;
+		if (this.x<0) {
+			this.xDirection *= -1;
+			this.x = 0;
+		} else if (this.x>cWidth) {
+			this.xDirection *= -1;
+			this.x = cWidth;
+		}
+		
+		this.y += this.yDirection*this.speed*modifier;
+		if (this.y<0) {
+			this.yDirection *= -1;
+			this.y = 0;
+		} else if (this.y>cHeight) {
+			this.yDirection *= -1;
+			this.y = cHeight;			
+		}
+		
+		ctx.beginPath();		
+		ctx.fillStyle="#f08047";
+		ctx.arc(this.x, this.y,this.r,0,2*Math.PI);
+		ctx.fill();
+		ctx.strokeStyle="#000";
+		ctx.lineWidth = 1;
+		ctx.stroke();
+		
+	}
+}
+
+function drawCoins() {
+	var r = cHeight*0.035;
+	ctx.textBaseline="middle";
+	
+	ctx.beginPath();
+	ctx.fillStyle="#ffff80";
+	ctx.strokeStyle="#f08047";
+	ctx.lineWidth = 1;
+	ctx.arc(2*r, cHeight - r*2, r,0,2*Math.PI);
+	ctx.fill();
+	ctx.stroke();
+	
+	ctx.textAlign="center";
+	ctx.fillStyle = "#f08047";
+	ctx.font = 1.2*r + "px Monospace";
+	ctx.fillText('$', r*2, cHeight - r*2);
+	
+	ctx.beginPath();
+	ctx.textAlign="left";
+	ctx.fillStyle = "#8db0d3";
+	ctx.font = 2*r + "px Monospace";
+	ctx.fillText(' ' + localStorage.coins, r*3.8, cHeight - r*2);
+	
+	ctx.fillStyle = "#7bd827";
+	ctx.fillText('x', r*3.5, cHeight - r*2);
+}
+
+function drawScore() {
+	var r = cHeight*0.025;
+	ctx.textBaseline="middle";
+	ctx.textAlign="center";
+	
+	ctx.beginPath();
+	ctx.font = 2*r + "px Monospace";
+	ctx.fillStyle = "#8db0d3";
+	ctx.fillText('Score ' + score, cWidth*0.5, r*2);
+	ctx.fillStyle = "#7bd827";
+	ctx.fillText('     :' + strToSpace(score), cWidth*0.5, r*2);
+}
+
+function drawNotification(msg){
+	if (msg) {
+		var r = cHeight*0.025;
+		ctx.textBaseline="middle";
+		ctx.textAlign="center";
+		
+		ctx.beginPath();
+		ctx.font = 4*r + "px Monospace";
+		ctx.fillStyle = "#ccc";
+		ctx.fillText(msg, cWidth*0.5, cHeight*0.5 - r*3);
+		ctx.fillStyle = "#7bd827";
+		ctx.fillText('|' + strToSpace(msg) + '|', cWidth*0.5, cHeight*0.5 - r*3);
+	}
+}
+
+function strToSpace(str){
+	var aux = '';
+	for (i=0; i<str.toString().length; i++)
+		aux+=' ';
+	return aux;
+}
+
+var getPhoneGapPath = function() {
+		var path = window.location.pathname;
+		path = path.substr( path, path.length - 10 );
+		return path;
+};
+//background snd
+function onSuccess() {}
+function onError(error) {}
+function onStatus(status) {
+		if( status==Media.MEDIA_STOPPED ) {
+				snd.play();
+		}
+}
+
+function startGame() {
+	enemies.length = 0;
+	player = new ball(cWidth*0.5, cHeight*0.5);
+	t = null;
+	c = null;
+	notification = 'tap the screen to move';
+	score = 0;
+	appPaused = false;
+	
+	then = Date.now();
+	loop();
+}
+
+function fontFix(){
+	globalR = cHeight*0.025;
+	
+	$('body').css({'fontSize' : globalR*2});
+	$('#menu ul .fa').css({'width' : globalR*4});
+	$('#bomb').css({'width' : globalR*5.5, 'height' : globalR*4.5 });
+	$('#bomb div').css({'width' : globalR*3, 'height' : globalR*2.5, 'paddingTop' : globalR*0.5});
+	$('#centerMenu').css({'margin-top' : cHeight*0.5-$('#centerMenu').height()*0.5});
+}
+
+function doLogin() {
+	if (typeof(googleplaygame) != "undefined") {
+		googleplaygame.isSignedIn(function (result) {
+			if (result.isSignedIn) {
+				// show user if logged in
+				googleplaygame.showPlayer(function (playerData) {
+					$('.hidden').fadeIn(0);
+				});
+				submitHighscore(localStorage.highscore);
+			} else {
+				// login and then show user
+				googleplaygame.auth(function(){
+					googleplaygame.showPlayer(function (playerData) {
+						localStorage.isLogin = 'true';
+						$('.hidden').fadeIn(0);
+					});
+					submitHighscore(localStorage.highscore);
+				}, function(){
+					alert("Can't connect to the internet. Your score will not be saved on our leaderboard.");
+					$('#autoLogin .fa').removeClass('fa-user').addClass('fa-user-times');
+					$('#autoLogin span').html('off');
+					localStorage.isLogin = 'false';
+				});
+			}
+		});
+	}
+}
+
+function doAchievement(aID){
+	if (localStorage.isLogin == 'true' && typeof(googleplaygame) != "undefined") {
+		var data = {
+			achievementId: aID
+		};
+		googleplaygame.unlockAchievement(data);
+	}
+}
+
+function submitHighscore(x) {
+	if (typeof(googleplaygame) != "undefined") {
+		var data = {
+			score: x,
+			leaderboardId: "CgkI_7ufk-EKEAIQAQ"
+		};
+		googleplaygame.submitScore(data);
+	}
+}
